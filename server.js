b2f6d8b8f64287d1f0c0dbd126f84cfd9f54567e
retheesh-g <retheesh.gopinathamenon@gmail.com>
2016-09-24 15:43:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,37 +23,44 @@ var articleOne = {
         </p>`
 };
 
-var htmlTemplate = `
-    <html>
-    <head>
-        <title>
-           ${title}
-        </title>
-        <meta name="viexport" content="width=device-width, initial=scale-1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-            <div>
-                <a href='/'>Home</a>
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `
+        <html>
+        <head>
+            <title>
+               ${title}
+            </title>
+            <meta name="viexport" content="width=device-width, initial=scale-1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href='/'>Home</a>
+                </div>
+                <hr/>
+                
+                <h3>
+                  ${heading}
+                </h3>
+                
+                <div>
+                  ${date}
+                </div>
+                
+                <div>
+                    ${content}
+                </div>
             </div>
-            <hr/>
-            
-            <h3>
-              ${heading}
-            </h3>
-            
-            <div>
-              ${date}
-            </div>
-            
-            <div>
-                ${content}
-            </div>
-        </div>
-    </body>
-</html>
-`;
+        </body>
+    </html>
+    `;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
